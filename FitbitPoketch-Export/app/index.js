5a933533ca89a974868ddc51a9b14ef1cc48e9b4
Skiy <skiy@outlook.com>
2021-08-26 18:17:39 +0100

    feat: simple watch face

--- FitbitPoketch-Export/app/index.js
+++ FitbitPoketch-Export/app/index.js
@@ -7,10 +7,17 @@ console.log("App code started");
 import clock from "clock";
 import * as document from "document";
 
-clock.granularity = "seconds"; // seconds, minutes, hours
+clock.granularity = "minutes"; // seconds, minutes, hours
 
-const clockLabel = document.getElementById("clock-label");
+const hourTen = document.getElementById("hour_ten");
+const hourOne = document.getElementById("hour_one");
+const minTen = document.getElementById("min_ten");
+const minOne = document.getElementById("min_one");
 
 clock.addEventListener("tick", (evt) => {
-  clockLabel.text = evt.date.toTimeString().slice(0, -4);
+  let timeString = evt.date.toTimeString().slice(0, 5);
+  hourTen.href = "icons/digit_" + timeString[0] + ".png";
+  hourOne.href = "icons/digit_" + timeString[1] + ".png";
+  minTen.href = "icons/digit_" + timeString[3] + ".png";
+  minOne.href = "icons/digit_" + timeString[4] + ".png";
 });
