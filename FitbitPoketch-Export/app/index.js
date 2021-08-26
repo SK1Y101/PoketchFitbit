26e231f7260840500782071964fefdc3a5a35839
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2021-08-26 02:00:49 +0100

    feat: basic user interface planning underway

--- FitbitPoketch-Export/app/index.js
+++ FitbitPoketch-Export/app/index.js
@@ -3,3 +3,14 @@
  */
 
 console.log("App code started");
+
+import clock from "clock";
+import * as document from "document";
+
+clock.granularity = "seconds"; // seconds, minutes, hours
+
+const clockLabel = document.getElementById("clock-label");
+
+clock.addEventListener("tick", (evt) => {
+  clockLabel.text = evt.date.toTimeString().slice(0, -4);
+});
