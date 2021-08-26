85827d8e0fc93db0c410a799f96ccddaaf4c36eb
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2021-08-26 22:04:27 +0100

    feat: can change the face colour of the watch

--- FitbitPoketch-Export/settings/index.jsx
+++ FitbitPoketch-Export/settings/index.jsx
@@ -22,11 +22,27 @@ function mySettings(props) {
           label={"Edge Colour"}
           settingsKey="edgeColour"
           options={[
-            {name: "D/P Blue", icon:"https://www.colorhexa.com/3050F8.png", value: 0, subname:"The Male Pokétch colour in in Diamond and Pearl."},
-            {name: "D/P Pink", icon:"https://www.colorhexa.com/FF41BC.png", value: 1, subname:"The Female Pokétch colour in in Diamond and Pearl."},
-            {name: "D/P Orange", icon:"https://www.colorhexa.com/E28B55.png", value: 2, subname:"The Rivals Pokétch colour in in Diamond and Pearl."},
-            {name: "Pt Blue", icon:"https://www.colorhexa.com/4C8CB9.png", value: 3, subname:"The Male Pokétch colour in Platinum."},
-            {name: "Pt Pink", icon:"https://www.colorhexa.com/EC5E6A.png", value: 4, subname:"The Female Pokétch colour in Platinum."},
+            {name: "D/P Blue", icon:"https://www.colorhexa.com/3050F8.png", value: "#3050F8", subname:"The Male Pokétch colour in Diamond and Pearl."},
+            {name: "D/P Pink", icon:"https://www.colorhexa.com/FF41BC.png", value: "#FF41BC", subname:"The Female Pokétch colour in Diamond and Pearl."},
+            {name: "D/P Orange", icon:"https://www.colorhexa.com/E28B55.png", value: "#E28B55", subname:"The Rivals Pokétch colour in Diamond and Pearl."},
+            {name: "Pt Blue", icon:"https://www.colorhexa.com/4C8CB9.png", value: "#4C8CB9", subname:"The Male Pokétch colour in Platinum."},
+            {name: "Pt Pink", icon:"https://www.colorhexa.com/EC5E6A.png", value: "#EC5E6A", subname:"The Female Pokétch colour in Platinum."},
+          ]}
+          renderItem={
+            (option) =>
+              <TextImageRow
+                label={option.name}
+                sublabel={option.subname}
+                icon={option.icon}
+              />
+          }
+        />
+        <Select
+          label={"Face Colour"}
+          settingsKey="faceColour"
+          options={[
+            {name: "D/P Grey", icon:"https://www.colorhexa.com/303030.png", value: "#303030", subname:"The Pokétch face colour in Diamond and Pearl."},
+            {name: "Pt White", icon:"https://www.colorhexa.com/E0E0E0.png", value: "#E0E0E0", subname:"The Pokétch face colour in Platinum."},
           ]}
           renderItem={
             (option) =>
