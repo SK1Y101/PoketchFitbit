51f0c7d442601b9874de28ae16d5585fcd9ee601
Skye Owen-Lloyd-Walters <skiy@outlook.com>
2021-08-26 20:32:31 +0100

    feat: implemented colour changing from the settings

---
+++ FitbitPoketch-Export/companion/setdefaults.js
@@ -0,0 +1,31 @@
+//Import the inbuilt modules
+import { settingsStorage } from "settings";
+
+function fetchdefs() {
+  var defaults = {
+    skin: {name:"Platinum", value:1, subname:"The Pokétch given to the protagonist in Platinum."},
+    edgeColour: {name: "D/P Blue", icon:"https://www.colorhexa.com/3050F8.png", value: 0, subname:"The Male Pokétch colour in in Diamond and Pearl."},
+  };
+
+  let Defaults = {};
+  for (var key in defaults) {
+    let val = defaults[key];
+    if (typeof val === "boolean") {
+      Defaults[key] = val;
+    } else {
+      Defaults[key] = {values:[{name: val["name"], value: val["value"]}],selected:val["selected"]};
+    };
+  };
+  return Defaults;
+};
+
+export function init() {
+  let defs = fetchdefs();
+  for (let i in defs) {
+    let exp = settingsStorage.getItem(i);
+    if (exp === null) {
+      settingsStorage.setItem(i, JSON.stringify(defs[i]));
+    };
+  };
+  console.log("loaded defaults for companion");
+};
