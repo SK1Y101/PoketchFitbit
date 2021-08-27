c509027fcc911621ed4713d0f5b4164d673b844b
Skye Owen-Lloyd-Walters <skiy@outlook.com>
2021-08-27 17:44:45 +0100

    watch face can now handle multiple views

--- FitbitPoketch-Export/app/Poketch/switch.js
+++ FitbitPoketch-Export/app/Poketch/switch.js
@@ -24,6 +24,41 @@ export let SwitchView = function(doc) {
   // Variable to store if the screen is updating
   var updating = false;
 
+  // Store the view information
+  var views = {};
+  var vnum = 0;
+  var vlen = 0;
+
+  // Function to fetch the available views
+  let fetchViews = function() {
+    do {
+      // fetch the next view
+      views[vnum] = doc.getElementById("view"+vnum);
+      ++vnum;
+      ++vlen;
+    } while (doc.getElementById("view"+vnum) != null);
+    vnum=0;
+  };
+
+  // Function to switch to the next view
+  let changeView = function(inc=0) {
+    // compute the new view to change to
+    var vnew = (vnum + inc + vlen) % vlen;
+    console.log("new view: "+vnew, "old view: "+vnum);
+    // If it is different to the current one
+    if (vnew != vnum) {
+      // change the display visibility of the elements
+      views[vnum].style.display = "none";
+      views[vnew].style.display = "inline";
+      // And update the view number
+      vnum = (vnum + inc + vlen) % vlen;
+    };
+    console.log("vnum is now:"+vnum);
+  };
+
+  // And fetch all of the views
+  fetchViews();
+
   // Create event listeners
 
   // foreward button pressed
@@ -51,9 +86,15 @@ export let SwitchView = function(doc) {
       // start the animations
       animateElement(view, "enable");
       animateElement(downBut, "mouseup");
-      // And force the ui to stop until the animations are done
+      // force the ui to stop until the animations are done
       updating = true;
-      setTimeout(function() { updating = false; }, 550 + 250);
+      // wait until the view is greyed out
+      setTimeout(function() {
+        //change the view
+        changeView(1);
+        // and set a timout to open the display again
+        setTimeout(function() { updating = false; }, 400);
+      }, 400 );
     };
   });
 
@@ -64,9 +105,15 @@ export let SwitchView = function(doc) {
       // start the animations
       animateElement(view, "enable");
       animateElement(upBut, "mouseup");
-      // And force the ui to stop until the animations are done
+      // force the ui to stop until the animations are done
       updating = true;
-      setTimeout(function() { updating = false; }, 550 + 250);
+      // wait until the view is greyed out
+      setTimeout(function() {
+        //change the view
+        changeView(-1);
+        // and set a timout to open the display again
+        setTimeout(function() { updating = false; }, 400);
+      }, 400 );
     };
   });
 };
