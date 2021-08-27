23eb96001b44312fd8de62a94ad84170cb4b0dfb
Skiy <skiy@outlook.com>
2021-08-27 15:15:16 +0100

    feat: ui now includes the pokemon sprite

--- FitbitPoketch-Export/app/Poketch/clock.js
+++ FitbitPoketch-Export/app/Poketch/clock.js
@@ -4,9 +4,20 @@
 // Change a digit display
 function showDigit(ele, digit) {
   try {
-    ele.href = "icons/digit_" + digit + ".png";
+    ele.href = "digits/digit_" + digit + ".png";
   } catch(err) {
     console.log(err + ": Couldn't assign digit '" + digit + "'")
+  };
+};
+
+// Hide a set of gui elemetns
+function showElement(ele, val) {
+  try {
+    ele.forEach(function(eles) {
+      eles.style.display = (val ? "inline" : "none");
+    });
+  } catch(err) {
+    ele.style.display = (val ? "inline" : "none");
   };
 };
 
@@ -23,18 +34,25 @@ export let TimeIndicator = function(doc) {
   const minsTen = doc.getElementById("min_ten");
   const minsOne = doc.getElementById("min_one");
 
+  // Fetch the pikachu sprite
+  const daySprite = doc.getElementsByClassName("day")
+  const nightSprite = doc.getElementsByClassName("night")
 
   // Function to update the time
   this.drawTime = function(now) {
     // Fetch the time elements
     var hour = pad(now.getHours());
     var mins = pad(now.getMinutes());
+    var daytime = Math.abs(15 - now.getHours()) < 5;
 
-    // And update the parts that need it
-    console.log(now.getMonth().toString())
+    // update the time elements
     showDigit(hourTen, hour[0]);
     showDigit(hourOne, hour[1]);
     showDigit(minsTen, mins[0]);
     showDigit(minsOne, mins[1]);
+
+    // And update the sprite
+    showElement(daySprite, daytime);
+    showElement(nightSprite, !daytime);
   };
 };
