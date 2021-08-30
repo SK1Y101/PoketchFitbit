1733dec29071bac2d3a15e9e9729fabe5c49c1f8
Skye Owen-Lloyd-Walters <skiy@outlook.com>
2021-08-30 13:55:52 +0100

    fix: calendar supports user preference on first day of week

--- FitbitPoketch-Export/app/Poketch/calendar.js
+++ FitbitPoketch-Export/app/Poketch/calendar.js
@@ -1,4 +1,5 @@
 // Import the fitbit builtins
+import { preferences } from "user-settings";
 
 // Define any helper functions
 import * as utils from "../../common/utils";
@@ -13,6 +14,12 @@ export let CalendarView = function(doc) {
   const daySelect = doc.getElementById("calendar_day");
   // Month mover
   const weekSelect = doc.getElementById("calendar_week");
+  // Week start shower
+  const monStart = doc.getElementById("monday_start");
+  const sunStart = doc.getElementById("sunday_start");
+  // day obscurers
+  const botObs = doc.getElementById("calendar_bot_obs");
+  const endObs = doc.getElementById("calendar_end_obs");
 
   // Variables to access to check if the calendar has been initialised
   this.init = false;
@@ -20,17 +27,23 @@ export let CalendarView = function(doc) {
   // Function to update the time
   this.drawTime = function(now) {
     // Fetch the time elements
-    var mon = now.getMonth() + 1;
-    var date = now.getDate();
-    var day = now.getDay();
+    var mon = 10;//now.getMonth() + 1;
+    var date = 26;//now.getDate();
+    var day = 2;//now.getDay();
+    // fetch the first day of the week (user preference)
+    var fdw = preferences.firstDayOfWeek;
     // and the first day of the month
     var fday = new Date(now.getFullYear(), mon - 1, 1).getDay();
     // Fiddle with the month to make it work with the display at the top
     mon = (mon > 10) ? mon+"_" : mon;
+    // And offset all of the days by the user prefered weekday begining
+    day = ((day - fdw + 7)%7); fday = ((fday - fdw + 7)%7)
 
     // update the elements
     // Month at the top of screen
     digitHandler.update(mon);
+    utils.showElement(monStart, fdw);
+    utils.showElement(sunStart, !fdw);
 
     // Weekday display
     weekSelect.href = "icons/calendar_"+fday+".png";
