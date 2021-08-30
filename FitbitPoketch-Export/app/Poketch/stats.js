016554ed9aa495405579b586d6c6410e154a3249
Skiy <skiy@outlook.com>
2021-08-30 18:31:07 +0100

    feat: added bars that can be interacted with programatically

--- FitbitPoketch-Export/app/Poketch/stats.js
+++ FitbitPoketch-Export/app/Poketch/stats.js
@@ -1,5 +1,6 @@
 // Import the fitbit builtins
 import { me } from "appbit";
+import { battery } from "power";
 import * as activity from "user-activity";
 
 // Define any helper functions
@@ -7,23 +8,26 @@ import * as utils from "../../common/utils";
 
 // Define this module
 export let StatsIndicator = function(doc, settings) {
+  // Fetch ui Elements
+  const chargeBar = doc.getElementById("charge_bar");
+
   // function to draw the steps on screen
   this.draw = function() {
+    chargeBar.width = .82 * Math.floor(battery.chargeLevel);
     // Check we have permissions
     if (me.permissions.granted("access_activity")) {
       // Fetch the number of steps
       var steps = activity.today.adjusted.steps;
-      // update the offset on long press
-      if ((offset == -1) || (offset > steps))   { newOffset(steps); };
       // And show the desired step counter
-      if (stepView == 0) {
-        digitHandler.update(steps);
-      } else {
-        digitHandler.update(steps - offset);
-      };
     } else {
-      // Otherwise, draw nothing
-      digitHandler.update("-----");
+      // Otherwise
     };
   };
 };
+
+// Planning
+
+// Watch charge
+// Heart rate?
+// Distance traveled
+// AZM?
