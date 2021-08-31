ce64e306dc00ac033231d022cf30c334bfed55e9
Skiy <skiy@outlook.com>
2021-08-31 19:08:21 +0100

    feat: step stats and icon filling on goal reach

--- FitbitPoketch-Export/app/Poketch/stats.js
+++ FitbitPoketch-Export/app/Poketch/stats.js
@@ -11,6 +11,7 @@ export let StatsIndicator = function(doc, settings) {
   // Fetch ui Elements
   // percentage bars
   const chargeBar = doc.getElementById("charge_bar");
+  const stepBar = doc.getElementById("step_bar");
   const distBar = doc.getElementById("dist_bar");
   const calsBar = doc.getElementById("cals_bar");
   const eleBar = doc.getElementById("ele_bar");
@@ -18,35 +19,38 @@ export let StatsIndicator = function(doc, settings) {
 
   // text overlay
   const chargeTxt = doc.getElementById("charge_txt");
+  const stepTxt = doc.getElementById("step_txt");
   const distTxt = doc.getElementById("dist_txt");
   const calsTxt = doc.getElementById("cals_txt");
   const eleTxt = doc.getElementById("ele_txt");
   const azmTxt = doc.getElementById("azm_txt");
 
   // Icons
-  /*const chargeIcon = doc.getElementsByClassName("charge_icon");
-  const distIcon = doc.getElementsByClassName("dist_icon");
-  const calsIcon = doc.getElementsByClassName("cals_icon");
-  const eleIcon = doc.getElementsByClassName("ele_icon");
-  const azmIcon = doc.getElementsByClassName("azm_icon");
-  */
+  const chargeIcon = doc.getElementById("charge_icon");
+  const stepIcon = doc.getElementById("step_icon");
+  const distIcon = doc.getElementById("dist_icon");
+  const calsIcon = doc.getElementById("cals_icon");
+  const eleIcon = doc.getElementById("ele_icon");
+  const azmIcon = doc.getElementById("azm_icon");
 
   // Function to set the properties correctly
-  let setStat = function(bar, txt, value, maxval, unit="", text="") {
-    bar.width = 78 * Math.min(1, value / maxval);
+  let setStat = function(bar, txt, value, maxval=100, unit="%", icon=null) {
+    bar.width = Math.min(value / maxval, 1) * 78;
     txt.text = value + " " + unit;
+    if (icon) { icon.style.display = (value >= maxval) ? "inline" : "none"; };
   };
 
   // function to draw the steps on screen
   this.draw = function() {
     // Update the battery percentage (as it doesn't need permissions)
-    setStat(chargeBar, chargeTxt, battery.chargeLevel, 100, "%");
+    setStat(chargeBar, chargeTxt, battery.chargeLevel);
     // Check we have permissions
     if (me.permissions.granted("access_activity")) {
-      setStat(distBar, distTxt, today.adjusted.distance, goals.distance, "m");
-      setStat(calsBar, calsTxt, today.adjusted.calories, goals.calories, "kcal");
-      setStat(eleBar,  eleTxt,  today.adjusted.elevationGain, goals.elevationGain, "floors");
-      setStat(azmBar,  azmTxt,  today.adjusted.activeZoneMinutes.total, goals.activeZoneMinutes.total, "mins");
+      setStat(stepBar, stepTxt, today.adjusted.steps, goals.steps, "m", stepIcon);
+      setStat(distBar, distTxt, today.adjusted.distance, goals.distance, "m", distIcon);
+      setStat(calsBar, calsTxt, today.adjusted.calories, goals.calories, "kcal", calsIcon);
+      setStat(eleBar,  eleTxt,  today.adjusted.elevationGain, goals.elevationGain, "floors", eleIcon);
+      setStat(azmBar,  azmTxt,  today.adjusted.activeZoneMinutes.total, goals.activeZoneMinutes.total, "mins",  azmIcon);
     } else {
       // Otherwise
     };
