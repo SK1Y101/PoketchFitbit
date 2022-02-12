a1daf6433b0b35e1fe2986af0ab9d32a9561cc84
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2022-02-12 15:54:03 +0000

    change text on type buttons

--- FitbitPoketch-Export/SDK4.3/app/Poketch/typecalc.js
+++ FitbitPoketch-Export/SDK4.3/app/Poketch/typecalc.js
@@ -6,42 +6,42 @@ import * as utils from "../../common/utils";
 export let TypeCalc = function(doc) {
 
   // fetch the button container
-  const moveTypeBut = doc.getElementsByClassName("move_type_button")
+  //const moveTypeBut = doc.getElementsByClassName("move_type_button")
 
   // Fetch the text on a button
   const moveTypeText = doc.getElementById("move_type_text");
   moveTypeText.text = "Normal";
 
   // And the actual trigger
-  const mtb = doc.getElementById("move_type_but");
+  //const mtb = doc.getElementById("move_type_but");
   // Move the trigger to layer 110 so that it is above the view change buttons
-  utils.changeLayer(mtb, 150);
+  //utils.changeLayer(mtb, 150);
 
   // the type as a number
-  const numToType = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"];
-  const numTypes = numToType.length;
+  //const numToType = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"];
+  //const numTypes = numToType.length;
 
   // Fetch the selected move type
-  var moveTypeDisplay = 0; //settings.getOrElse("MoveTypeValue", 0);
+  //var moveTypeDisplay = 0; //settings.getOrElse("MoveTypeValue", 0);
 
   // function to draw the display
   this.draw = function() {
     // update the types
-    moveTypeText.text = numToType[moveTypeDisplay];
+    //moveTypeText.text = numToType[moveTypeDisplay];
     // and save the value
-    settings.replaceSettings({"MoveTypeValue":moveTypeDisplay});
+    //settings.replaceSettings({"MoveTypeValue":moveTypeDisplay});
   };
 
   // increment the move type on click
-  mtb.addEventListener("click", (evt) => {
-    // increment the type
-    moveTypeDisplay = (moveTypeDisplay + 1 + numTypes) % numTypes;
-    // animate the click
-    utils.animateElement(moveTypeBut, "click");
-    // and update the display
-    this.draw();
-  });
+  //mtb.addEventListener("click", (evt) => {
+  //  // increment the type
+  //  moveTypeDisplay = (moveTypeDisplay + 1 + numTypes) % numTypes;
+  //  // animate the click
+  //  utils.animateElement(moveTypeBut, "click");
+  //  // and update the display
+  //  this.draw();
+  //});
 
   // and draw on intitalisation
-  draw();
+  //draw();
 };
