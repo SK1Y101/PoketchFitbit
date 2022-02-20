770dfc91b390c6e944d63af7c3700d6daad69d4b
Skye Owen-Lloyd-Walters <skiy@outlook.com>
2022-02-20 23:21:15 +0000

    fix SDK6.0 update

--- FitbitPoketch-Export/SDK6.0/app/index.js
+++ FitbitPoketch-Export/SDK6.0/app/index.js
@@ -37,9 +37,12 @@ let DefSet = function() {
     activeApps: [1],
     // Pedometer
     stepView: 0,
-    stepView: 0,
     // counter
     counterValue: 0,
+    // secondInteract
+    secondInteract: 0,
+    longPressTime: "1000",
+    multiTapTime: "500",
   };
   return defaults;
 };
@@ -48,6 +51,7 @@ let DefSet = function() {
 String.prototype.splice = function(start, end, replacement) {
   return this.substr(0, start) + replacement + this.substr(end);
 }
+
 const debug = false;
 
 // And fetch a reference to the modules
@@ -70,13 +74,13 @@ var viewUpdate = {
 };
 
 // define the switch viewer, passing any updates needed
-let switchView = new SwitchView(document, settings, viewUpdate);
+let switchView = new SwitchView(document, settings, viewUpdate, debug);
 
 // Define the clock tick rate
 clock.granularity = "minutes"; // seconds, minutes, hours
 
 // fetch elementss
-const bg = document.getElementsByClassName("background");
+const bg = document.getElementById("background");
 const fc = document.getElementsByClassName("face_colour");
 const sc = document.getElementsByClassName("screen_colour");
 const dpskin = document.getElementsByClassName("dp_skin");
@@ -142,6 +146,30 @@ let updateSkin = function(skinType) {
   face.groupTransform.scale.y = (skinType==3 ? 100 / 93.5: 1);
 }
 
+// change how secondary buttons are handeled
+let updateSecondaryButtons = function(sec) {
+  var secVal = parseInt(sec);
+  countCounter.updateSecondary(secVal);
+  stepCounter.updateSecondary(secVal);
+  moveCalc.updateSecondary(secVal);
+};
+
+// long press time
+let updateLongPressTime = function(timeselect) {
+  var timeval = parseInt(timeselect);
+  countCounter.updateLongPress(timeval);
+  stepCounter.updateLongPress(timeval);
+  moveCalc.updateLongPress(timeval);
+};
+
+// multi tap time
+let updateMultiTapTime = function(timeselect) {
+  var timeval = parseInt(timeselect);
+  countCounter.updateMultiTap(timeval);
+  stepCounter.updateMultiTap(timeval);
+  moveCalc.updateMultiTap(timeval);
+};
+
 // Define a function to apply our settings
 let applySettings = function() {
   if (! settings) {
@@ -156,6 +184,10 @@ let applySettings = function() {
     settings.isPresent("activeApps", switchView.appSettings);
     // Choose the mascot
     settings.isPresent("mascotSprite", timeInd.switchMascot);
+    // set secondary buttons
+    settings.isPresent("secondInteract", updateSecondaryButtons);
+    settings.isPresent("longPressTime", updateLongPressTime);
+    settings.isPresent("multiTapTime", updateMultiTapTime);
     // Show that settings have been loaded
     console.log("Settings applied");
   } catch (err) {
